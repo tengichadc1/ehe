89ffce74945a5e2b000ae8e80de4091630f7fd90
tengichadc1 <91111179+tengichadc1@users.noreply.github.com>
2021-09-21 12:22:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Chao Xìn bé iu',
+    introDesc: `Anh có điều này muốn hỏi Em, k trả lời làm chóa`,
+    btnIntro: 'OKn"t'
+    title: 'Anh biết là Em yêu Anh lắm luon phải khong ?',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thừa nhận đó =)) ',
+    btnYes: 'Có, em thật sự rất rất ấy anh 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em iu anh',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh iu em đóa Ahiiiii UwU',
+    mess: 'Anh biết mà, nhưng mà lý do anh iu em nó khác cơ ><',
+    messDesc: 'ib cho a biết lý do thật sự đi :33',
+    btnAccept: 'yes"t',
+    messLink: 'https://www.facebook.com/quy.vo.587606'
+}
